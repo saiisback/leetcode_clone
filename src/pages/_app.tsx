7493708ef6fa4c1b9567891c51fe7bfa388b9978
sai karthik <69042752+saiisback@users.noreply.github.com>
2024-07-22 16:11:09 +0000

    update 0.5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <head>
+        <title>CODE CRAFT</title>
+        <meta name="viewport" content="width=device-width,inital-scale=1"/>
+        <link rel="icon" href="pfp.png"/>
+        <meta name="description" content="Code craft"/>
+      </head>
+    <Component {...pageProps} />
+    </>
+  );
 }
