91511efad517b048d8084fa1474616d938d56328
sai karthik <69042752+saiisback@users.noreply.github.com>
2024-07-22 15:30:49 +0000

    update 0.3

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type navbarProps = {
+    
+};
+
+const navbar:React.FC<navbarProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default navbar;
